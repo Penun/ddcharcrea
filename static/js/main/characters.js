ef0ce8e757844622a7dd5d038b8b77980bc4077c
Damon <damon.frankfurth@gmail.com>
2016-08-18 21:25:18 -0600

    Added skills panel and base calcualtion logic for front end display.

--- static/js/main/characters.js
+++ static/js/main/characters.js
@@ -1,17 +1,91 @@
 (function(){
 	var app = angular.module('ddchar_characters', []);
 	app.controller('charController', ['$scope', function($scope){
-		this.RevealDetails = function(ch_id){
+		this.RevealDetails = function(playchar_id){
 			for (var i = 0; i < $scope.playchars.length; i++){
-				if ($scope.playchars[i].ch_id == ch_id){
+				if ($scope.playchars[i].playchar_id === playchar_id){
 					if ($scope.playchars[i].showDetails == null || !$scope.playchars[i].showDetails) {
 						$scope.playchars[i].showDetails = true;
 						break;
 					} else {
 						$scope.playchars[i].showDetails = false;
+						break;
 					}
 				}
 			}
 		};
+
+		this.RevealSkills = function(playchar_id){
+			for (var i = 0; i < $scope.playchars.length; i++){
+				if ($scope.playchars[i].playchar_id === playchar_id){
+					if ($scope.playchars[i].showDetails && ($scope.playchars[i].showSkills == null || !$scope.playchars[i].showSkills)){
+						if ($scope.playchars[i].showSkills == null){
+							this.CalculateSkills(i);
+						}
+						$scope.playchars[i].showSkills = true;
+						break;
+					} else {
+						$scope.playchars[i].showSkills = false;
+						break;
+					}
+				}
+			} 
+		};
+
+		this.CalculateSkills = function(playchar_i){
+			var m_str = 0;
+			var m_dex = 0;
+			var m_int = 0;
+			var m_wis = 0;
+			var m_cha = 0;
+			if ($scope.playchars[playchar_i].b_str < 10){
+				m_str = ($scope.playchars[playchar_i].b_str - 11) / 2;								
+			} else {
+				m_str = ($scope.playchars[playchar_i].b_str - 10) / 2;
+			}
+			if ($scope.playchars[playchar_i].b_dex < 10){
+				m_dex = ($scope.playchars[playchar_i].b_dex - 11) / 2;								
+			} else {
+				m_dex = ($scope.playchars[playchar_i].b_dex - 10) / 2;
+			}
+			if ($scope.playchars[playchar_i].b_int < 10){
+				m_int = ($scope.playchars[playchar_i].b_int - 11) / 2;								
+			} else {
+				m_int = ($scope.playchars[playchar_i].b_int - 10) / 2;
+			}
+			if ($scope.playchars[playchar_i].b_wis < 10){
+				m_wis = ($scope.playchars[playchar_i].b_wis - 11) / 2;								
+			} else {
+				m_wis = ($scope.playchars[playchar_i].b_wis - 10) / 2;
+			}
+			if ($scope.playchars[playchar_i].b_cha < 10){
+				m_cha = ($scope.playchars[playchar_i].b_cha - 11) / 2;								
+			} else {
+				m_cha = ($scope.playchars[playchar_i].b_cha - 10) / 2;
+			}
+			m_str = Math.floor(m_str);
+			m_dex = Math.floor(m_dex);
+			m_int = Math.floor(m_int);
+			m_wis = Math.floor(m_wis);
+			m_cha = Math.floor(m_cha);							
+			$scope.playchars[playchar_i].acro = m_dex;
+			$scope.playchars[playchar_i].anim = m_wis;
+			$scope.playchars[playchar_i].arca = m_int;
+			$scope.playchars[playchar_i].athl = m_str;
+			$scope.playchars[playchar_i].dece = m_cha;
+			$scope.playchars[playchar_i].hist = m_int;
+			$scope.playchars[playchar_i].insi = m_wis;
+			$scope.playchars[playchar_i].inti = m_cha;
+			$scope.playchars[playchar_i].inve = m_int;
+			$scope.playchars[playchar_i].medi = m_wis;
+			$scope.playchars[playchar_i].natu = m_int;
+			$scope.playchars[playchar_i].perc = m_wis;
+			$scope.playchars[playchar_i].perf = m_cha;
+			$scope.playchars[playchar_i].pers = m_cha;
+			$scope.playchars[playchar_i].reli = m_int;
+			$scope.playchars[playchar_i].sloh = m_dex;
+			$scope.playchars[playchar_i].stea = m_dex;
+			$scope.playchars[playchar_i].surv = m_wis;
+		}
 	}]);
 })();
--- views/main/characters.tpl
+++ views/main/characters.tpl
@@ -1,12 +1,35 @@
 <div ng-repeat="char in playchars">
 	<div class="d_list">
-		<div ng-click="charCont.RevealDetails(char.ch_id)">
+		<div ng-click="charCont.RevealDetails(char.playchar_id)">
 			<div class="d_list_i"><b><i>{{"{{char.name}}"}}</i></b></div><div class="d_list_i"><b>{{"{{char.refClass}}"}}</b></div><div class="d_list_i"><b>{{"{{char.raceRef}}"}}</b></div><div class="d_list_i"><b>Lvl {{"{{char.level}}"}}</b></div><br />
 		</div>
 		<div ng-show="char.showDetails">
-			<div class="d_list_i"><b>HP {{"{{char.hit_points}}"}}</b></div><div class="d_list_i"><b>Exp {{"{{char.exp}}"}}</b></div><div class="d_list_i"><b>Sex {{"{{char.sex}}"}}</b></div><br />
-			<div class="d_list_i"><b>Str {{"{{char.b_str}}"}}</b></div><div class="d_list_i"><b>Dex {{"{{char.b_dex}}"}}</b></div><div class="d_list_i"><b>Con {{"{{char.b_con}}"}}</b></div><br />
-			<div class="d_list_i"><b>Int {{"{{char.b_int}}"}}</b></div><div class="d_list_i"><b>Wis {{"{{char.b_wis}}"}}</b></div><div class="d_list_i"><b>Cha {{"{{char.b_cha}}"}}</b></div><br />
+			<div class="d_list_i"><b>HP</b>: {{"{{char.hit_points}}"}}</div><div class="d_list_i"><b>Exp</b>: {{"{{char.exp}}"}}</div><div class="d_list_i"><b>Sex</b>: {{"{{char.sex}}"}}</div><br />
+			<div class="d_list_i"><b>Str</b>: {{"{{char.b_str}}"}}</div><div class="d_list_i"><b>Dex</b>: {{"{{char.b_dex}}"}}</div><div class="d_list_i"><b>Con</b>: {{"{{char.b_con}}"}}</div><br />
+			<div class="d_list_i"><b>Int</b>: {{"{{char.b_int}}"}}</div><div class="d_list_i"><b>Wis</b>: {{"{{char.b_wis}}"}}</div><div class="d_list_i"><b>Cha</b>: {{"{{char.b_cha}}"}}</div><br />
+			<div id="skill_panel" ng-click="charCont.RevealSkills(char.playchar_id)">
+				<h2>Skills</h2>
+				<div ng-show="char.showSkills">
+					<div><b>Acrobatics</b> <i>(Dex)</i>: {{"{{char.acro}}"}}</div>		
+					<div><b>Animal Handling</b> <i>(Wis)</i>: {{"{{char.anim}}"}}</div>		
+					<div><b>Arcana</b> <i>(Int)</i>: {{"{{char.arca}}"}}</div>		
+					<div><b>Athletics</b> <i>(Str)</i>: {{"{{char.athl}}"}}</div>		
+					<div><b>Deception</b> <i>(Cha)</i>: {{"{{char.dece}}"}}</div>		
+					<div><b>History</b> <i>(Int)</i>: {{"{{char.hist}}"}}</div>		
+					<div><b>Insight</b> <i>(Wis)</i>: {{"{{char.insi}}"}}</div>		
+					<div><b>Intimidation</b> <i>(Cha)</i>: {{"{{char.inti}}"}}</div>		
+					<div><b>Investigation</b> <i>(Int)</i>: {{"{{char.inve}}"}}</div>		
+					<div><b>Medicine</b> <i>(Wis)</i>: {{"{{char.medi}}"}}</div>		
+					<div><b>Nature</b> <i>(Int)</i>: {{"{{char.natu}}"}}</div>		
+					<div><b>Perception</b> <i>(Wis)</i>: {{"{{char.perc}}"}}</div>		
+					<div><b>Performance</b> <i>(Cha)</i>: {{"{{char.perf}}"}}</div>		
+					<div><b>Persuasion</b> <i>(Cha)</i>: {{"{{char.pers}}"}}</div>		
+					<div><b>Religion</b> <i>(Int)</i>: {{"{{char.reli}}"}}</div>		
+					<div><b>Sleight of Hand</b> <i>(Dex)</i>: {{"{{char.sloh}}"}}</div>		
+					<div><b>Stealth</b> <i>(Dex)</i>: {{"{{char.stea}}"}}</div>		
+					<div><b>Survival</b> <i>(Wis)</i>: {{"{{char.surv}}"}}</div>		
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
